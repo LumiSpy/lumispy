10cf4bf2704b9cb181ddd8c128cd2f3e7739ad55
jlaehne <jonas@pdi-berlin.de>
2019-11-18 21:27:06 +0100

    Some minor corrections, formatting and terminology.

--- lumispy/utils/__init__.py
+++ lumispy/utils/__init__.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright 2019 The LumiSpy developers
+# Copyright 2017-2019 The LumiSpy developers
 #
-# This file is part of lumispy.
+# This file is part of LumiSpy.
 #
-# lumispy is free software: you can redistribute it and/or modify
+# LumiSpy is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# lumispy is distributed in the hope that it will be useful,
+# LumiSpy is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with lumispy.  If not, see <http://www.gnu.org/licenses/>.
+# along with LumiSpy.  If not, see <http://www.gnu.org/licenses/>.
