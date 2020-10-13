ca999fb34360bf23b689dfa98a80a97c9f3a950f
Jordi <9082687+jordiferrero@users.noreply.github.com>
2020-10-13 15:18:16 +0100

    Update test_common_luminescence.py

--- lumispy/tests/test_common_luminescence.py
+++ lumispy/tests/test_common_luminescence.py
@@ -7,15 +7,6 @@ from lumispy.signals.luminescence_transient import LumiTransient
 
 class TestCommonLumi(TestCase):
 
-    # @pytest.mark.parametrize(
-    #     "nav_x, nav_y, output",
-    #     [
-    #         (10, 10, (6,6)),
-    #         (10, 15, (6,11)),
-    #         (2, 2, ValueError("The pixels to be cropped cannot be larger than half the width or the length!"))
-    #     ]
-    # )
-    #def test_crop_edges(self, nav_x, nav_y, output):
     def test_crop_edges(self):
         s1 = LumiSpectrum(np.ones((10, 10, 10)))
         s2 = LumiTransient(np.ones((10, 10, 10, 10)))
@@ -26,4 +17,4 @@ class TestCommonLumi(TestCase):
         assert s1.axes_manager.navigation_shape[1] == 6
         assert s2.axes_manager.navigation_shape[0] == 6
         assert s2.axes_manager.navigation_shape[1] == 6
-        #TestCase.assertRaises(ValueError, lambda:s3.crop_edges(2))
+        self.assertRaises(ValueError, s3.crop_edges, crop_px=2)
