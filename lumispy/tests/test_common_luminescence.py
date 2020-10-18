1845bd95cd268e7f504bd210b80eeee56d067764
Jordi <9082687+jordiferrero@users.noreply.github.com>
2020-10-18 11:13:41 +0100

    Standard formatting

--- lumispy/tests/test_common_luminescence.py
+++ lumispy/tests/test_common_luminescence.py
@@ -5,12 +5,13 @@ import pytest
 from lumispy.signals.luminescence_spectrum import LumiSpectrum
 from lumispy.signals.luminescence_transient import LumiTransient
 
+
 class TestCommonLumi(TestCase):
 
     def test_crop_edges(self):
         s1 = LumiSpectrum(np.ones((10, 10, 10)))
         s2 = LumiTransient(np.ones((10, 10, 10, 10)))
-        s3 = LumiSpectrum(np.ones((3,3,10)))
+        s3 = LumiSpectrum(np.ones((3, 3, 10)))
         s1 = s1.crop_edges(crop_px=2)
         s2 = s2.crop_edges(crop_px=2)
         assert s1.axes_manager.navigation_shape[0] == 6
