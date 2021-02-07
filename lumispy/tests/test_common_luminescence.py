99c7ec882924413d02bef535e3bde5eb0d5f6b13
jlaehne <jonas@pdi-berlin.de>
2021-02-07 23:03:34 +0100

    Add tests

--- lumispy/tests/test_common_luminescence.py
+++ lumispy/tests/test_common_luminescence.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+#from numpy import ones, random, all
 import numpy as np
 import pytest
 
@@ -19,3 +20,80 @@ class TestCommonLumi(TestCase):
         assert s2.axes_manager.navigation_shape[0] == 6
         assert s2.axes_manager.navigation_shape[1] == 6
         self.assertRaises(ValueError, s3.crop_edges, crop_px=2)
+
+    def test_remove_negative(self):
+        s1 = LumiSpectrum(np.random.random((10, 10, 10)))-0.3
+        s2 = LumiTransient(np.random.random((10, 10, 10, 10)))-0.3
+        s3 = LumiTransient(np.random.random((10, 10, 10, 10)))-0.3
+        s1a = s1.remove_negative(inplace=False)
+        s2a = s2.remove_negative(inplace=False)
+        s3a = s3.remove_negative(inplace=False,basevalue=0.1)
+        assert np.all(s1a.data[s1 <= 0] == 1)
+        assert np.all(s2a.data[s2 <= 0] == 1)
+        assert np.all(s3a.data[s3 <= 0] == 0.1)
+        s1.remove_negative(inplace=True)
+        s2.remove_negative(inplace=True)
+        s3.remove_negative(basevalue=0.1)
+        assert s1 == s1a
+        assert s2 == s2a
+        assert s3 == s3a
+
+    def test_scale_by_exposure(self):
+        # exposure=float('nan'), inplace=False
+        s1 = LumiSpectrum(np.ones((10, 10, 10)))
+        s2 = LumiTransient(np.ones((10, 10, 10, 10)))
+        s3 = LumiSpectrum(np.ones((10, 10)))
+        s4 = LumiSpectrum(np.ones((10)))
+        s2.metadata.set_item('Acquisition_instrument.CL.exposure', 2)
+        s3.metadata.set_item('Acquisition_instrument.CL.dwell_time', 0.5)
+        s1a = s1.scale_by_exposure(exposure=4)
+        s2a = s2.scale_by_exposure()
+        s3a = s3.scale_by_exposure()
+        s4a = s4.scale_by_exposure(exposure=0.1)
+        assert np.all(s1a.data == 0.25)
+        assert np.all(s2a.data == 0.5)
+        assert np.all(s3a.data == 2)
+        assert np.all(s4a.data == 10)
+        s1.scale_by_exposure(exposure=4, inplace=True)
+        s2.scale_by_exposure(inplace=True)
+        assert s1 == s1a
+        assert s2 == s2a
+
+    def test_normalize(self):
+        # pos=float('nan'), element_wise=False, inplace=False
+        s1 = LumiSpectrum(np.random.random((10, 10, 10)))*2
+        s2 = LumiTransient(np.random.random((10, 10, 10, 10)))*2
+        s3 = LumiSpectrum(np.random.random((10, 10)))*2
+        s4 = LumiSpectrum(np.random.random((10)))*2
+        s1a = s1.normalize()
+        s2a = s2.normalize()
+        s3a = s3.normalize()
+        s4a = s4.normalize()
+        assert s1a.max(axis = [0,1,2]).data[0] == 1
+        assert s2a.max(axis = [0,1,2,3]).data[0] == 1
+        assert s3a.max(axis = [0,1]).data[0] == 1
+        assert s4a.max(axis = [0]).data[0] == 1
+        s1a = s1.normalize(element_wise=True)
+        s2a = s2.normalize(element_wise=True)
+        s3a = s3.normalize(element_wise=True)
+        s4a = s4.normalize(element_wise=True)
+        assert np.all(s1a.max(axis = [2]).data[0] == 1)
+        assert np.all(s2a.max(axis = [3]).data[0] == 1)
+        assert np.all(s3a.max(axis = [1]).data[0] == 1)
+        assert s4a.max(axis = [0]).data[0] == 1
+        s1a = s1.normalize(pos=3)
+        s2a = s2.normalize(pos=3, element_wise=True)
+        s3a = s3.normalize(pos=3, element_wise=True)
+        s4a = s4.normalize(pos=3)
+        assert s1a.isig[3].max().data[0] == 1
+        assert np.all(s2a.isig[3] == 1)
+        assert np.all(s3a.isig[3] == 1)
+        assert s4a.isig[3].max().data[0] == 1
+        s1.normalize(pos=3, inplace=True)
+        s2.normalize(pos=3, element_wise=True, inplace=True)
+        s3.normalize(pos=3, element_wise=True, inplace=True)
+        s4.normalize(pos=3, inplace=True)
+        assert s1a == s1
+        assert s2a == s2
+        assert s3a == s3
+        assert s4a == s4
