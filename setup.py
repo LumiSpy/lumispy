9fcf5323c5f70355141bdbc9758f980543756d72
J. Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2021-03-26 17:44:25 +0000

    Updated version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ exec(open('lumispy/release_info.py').read())  # grab version info
 
 setup(
     name='lumispy',
-    version='0.1.0',
+    version='0.1.1',
     description='Luminescence spectroscopy data analysis with HyperSpy.',
     author='The LumiSpy Developers',
     #author_email=email,
