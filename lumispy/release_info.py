f2e0525dd4fb9bd5b80adcdddd9cfa9e1f1773a1
Jonas Lähnemann <jonas@pdi-berlin.de>
2021-04-27 10:04:32 +0200

    Update release_info.py

--- lumispy/release_info.py
+++ lumispy/release_info.py
@@ -1,9 +1,41 @@
 name = "lumispy"
-version = "0.1.1"
+version = "0.1.1dev"
+description = "Analysis of luminescence spectroscopy data with HyperSpy."
 author = "The LumiSpy developers"
 copyright = "Copyright 2019-2021, LumiSpy developers"
 credits = ["The LumiSpy developers"]
+url = "http://lumispy.org"
 license = "GPLv3"
+platforms = ['Linux', 'Mac OSX', 'Windows XP/2000/NT', 'Windows 95/98/ME']
 maintainer = "Jordi Ferrer Orri, Jonas Lähnemann, Duncan Johnstone"
 email = "jf631@cam.ac.uk"
 status = "Development"
+
+keywords = ['CL',
+            'PL',
+            'EL',
+            'TRCL',
+            'TRPL',
+            'SEM',
+            'STEM',
+            'TEM',
+            'cathodoluminescence',
+            'photoluminescence',
+            'electroluminescence',
+            'Raman',
+            'curve fitting',
+            'electron microscopy',
+            'hyperspectral',
+            'hyperspectrum',
+            'hyperspy',
+            'luminescence spectroscopy',
+            'lumispy',
+            'machine learning',
+            'model',
+            'multidimensional',
+            'numpy',
+            'python',
+            'scipy',
+            'spectroscopy',
+            'spectrum image',
+            'time-resolved']
