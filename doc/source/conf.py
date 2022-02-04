09bf0e8c08c4a0a6ec0e8d4703b98fd7c0ee248b
J. Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2022-02-04 12:04:30 +0000

    Try to fix AIP generation again 2

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,7 +40,7 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../media/logo_sq_april21.svg'
+html_logo = '../media/logo_rec_april21.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -49,3 +49,23 @@ html_favicon = '../media/lumispy.ico'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+
+def run_apidoc(_):
+    # https://www.sphinx-doc.org/en/master/man/sphinx-apidoc.html
+    # https://www.sphinx-doc.org/es/1.2/ext/autodoc.html
+    import os
+    os.environ['SPHINX_APIDOC_OPTIONS'] = 'members,private-members,no-undoc-members,show-inheritance,ignore-module-all'
+
+    from sphinx.ext.apidoc import main
+
+    cur_dir = os.path.normpath(os.path.dirname(__file__))
+    output_path = os.path.join(cur_dir, 'api')
+    modules = os.path.normpath(os.path.join(cur_dir, "../../lumispy"))
+    exclude_pattern = ["../../lumispy/tests",
+                       "../../lumispy/components",]
+    main(['-e', '-f', '-P', '-o', output_path, modules, *exclude_pattern])
+
+
+def setup(app):
+    app.connect('builder-inited', run_apidoc)
