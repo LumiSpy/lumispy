66d2309cbca628cd6fffed82fd22f3c53a4c54bd
J. Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2022-02-04 21:26:04 +0000

    Last change

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -46,7 +46,7 @@ html_css_files = ['custom.css']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../_static/logo_sq_april21.svg'
+html_logo = '../_static/logo_rec_april21.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
