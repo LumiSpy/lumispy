5afe6f872a161f0a5cce5101393031383379fcab
J. Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2022-04-29 17:00:21 +0100

    add depreciation warning

--- lumispy/signals/luminescence_spectrum.py
+++ lumispy/signals/luminescence_spectrum.py
@@ -512,6 +512,8 @@ class LumiSpectrum(Signal1D, CommonLumi):
         -----
         This function does not work with non-uniform axes.
         """
+        warnings.warn('The use of `remove_background_from_file` is depreciated. '
+                      'Please use `remove_backgroun` from Signal1D object', DeprecationWarning, stacklevel=2)
         if hasattr(self.metadata.Signal, "background_subtracted"):
             if self.metadata.Signal.background_subtracted is True:
                 raise RecursionError(
