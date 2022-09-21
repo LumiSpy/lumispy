65f08de51990493048bd06b0d68e04f480feec0a
Jordi Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2022-09-21 10:03:57 +0100

    lint fix again

--- lumispy/tests/signals/test_luminescence_spectrum.py
+++ lumispy/tests/signals/test_luminescence_spectrum.py
@@ -98,10 +98,6 @@ class TestLumiSpectrum:
         assert s.axes_manager.signal_axes[0].units == "nm"
 
         assert_allclose(s_copy.axes_manager.signal_axes[0].axis[0], 368.614, atol=0.1)
-        assert_allclose(
-            s_copy.axes_manager.signal_axes[0].axis[-1], 768.249, atol=0.1
-        )
+        assert_allclose(s_copy.axes_manager.signal_axes[0].axis[-1], 768.249, atol=0.1)
         assert_allclose(s.axes_manager.signal_axes[0].axis[0], 368.614, atol=0.1)
-        assert_allclose(
-            s.axes_manager.signal_axes[0].axis[-1], 768.249, atol=0.1
-        )
+        assert_allclose(s.axes_manager.signal_axes[0].axis[-1], 768.249, atol=0.1)
