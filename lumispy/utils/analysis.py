27b47f1e930aabcab304195bf55e9c6c948cc1d4
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2022-10-05 13:36:00 +0100

    added tests

--- lumispy/utils/analysis.py
+++ lumispy/utils/analysis.py
@@ -1,5 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019-2022 The LumiSpy developers
+#
+# This file is part of LumiSpy.
+#
+# LumiSpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the license, or
+# (at your option) any later version.
+#
+# LumiSpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LumiSpy. If not, see <https://www.gnu.org/licenses/#GPL>.
+
 import hyperspy.api as hs
-from warnings import warn
 
 
 def plot_span_map(sig, nspans=1):
@@ -21,21 +38,23 @@ def plot_span_map(sig, nspans=1):
 
     Returns
     -------
-    spans: [hs.roi.SpanROI]
+    all_sum: hs.signals.BaseSignal
+        Sum over all positions of `sig`, the 'navigator' for `plot_span_map`
+    spans: hs.roi.SpanROI
         The span ROIs from the navigator
-    span_sigs: [hs.signals.BaseSignal]
+    span_sigs: hs.signals.BaseSignal
         slices of `sig` according to each span roi
-    span_sums: [hs.signals.BaseSignal]
+    span_sums: hs.signals.BaseSignal
         the summed `span_sigs`.
     """
     if nspans > 3:
         raise ValueError("Maximum number of spans allowed is 3")
 
-    
     if sig.axes_manager.signal_dimension != 1 or sig.axes_manager.navigation_dimension != 2:
         sig_dims, nav_dims = sig.axes_manager.signal_dimension, sig.axes_manager.navigation_dimension
-        warn(f"This method is designed for data with 1 signal and 2 navigation dimensions, not {sig_dims} and {nav_dims} respectively"
-              " - therefore this function may misbehave")
+        raise ValueError(
+            f"This method is designed for data with 1 signal and 2 navigation dimensions, not {sig_dims} and {nav_dims} respectively"
+        )
 
     colors = ['red', 'green', 'blue']
     
@@ -48,8 +67,8 @@ def plot_span_map(sig, nspans=1):
     
     span_width = ax_sig_range / (2 * nspans)
     
-    all_s = sig.nansum()
-    all_s.plot()
+    all_sum = sig.nansum()
+    all_sum.plot()
     
     for i in range(nspans):
         # create a span that has a unique range 
@@ -60,14 +79,14 @@ def plot_span_map(sig, nspans=1):
         color = colors[i]
         
         # add it to the sum over all positions
-        span.add_widget(all_s, color=colors[i])
+        span.add_widget(all_sum, color=colors[i])
         
         # create a signal that is the spectral slice of sig
         span_sig = hs.interactive(span, signal=sig, event=span.events.changed, axes=sig.axes_manager.signal_axes)
         span_sigs.append(span_sig)
         
         # create a signal that is the spectral integral of span_sig
-        span_sum = span_sig.nansum(-1).as_signal2D([0, 1]) # convert to signal, otherwise the navigator doesn't support updating...?
+        span_sum = span_sig.nansum(-1).as_signal2D([0, 1]) # convert to 2D signal, otherwise the navigator doesn't support updating...?
         span_sums.append(span_sum)
         
         span_sum.plot(cmap=f'{color.capitalize()}s')
@@ -78,5 +97,5 @@ def plot_span_map(sig, nspans=1):
                out=span_sum)
 
     # return all ya bits for future messing around.
-    return spans, span_sigs, span_sums
+    return all_sum, spans, span_sigs, span_sums
 
