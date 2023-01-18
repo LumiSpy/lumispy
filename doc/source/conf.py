fb6b62014b622344fc06b7a4821510e9e6efd55b
Jonas Lähnemann <jonas@pdi-berlin.de>
2023-01-18 14:06:51 +0100

    replace imgmath by mathjax sphinx-extension

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.imgmath",
+    "sphinx.ext.mathjax",
     "sphinx.ext.graphviz",
     "sphinx.ext.autosummary",
     "sphinx_toggleprompt",
