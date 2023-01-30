1a885cb7d50849e5d40431925cf1770b79666e9f
Jordi Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2023-01-30 17:07:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,11 +60,7 @@ setup(
     extras_require={
         "tests": ["pytest>=5.0"],
         "coverage": ["pytest-cov", "codecov"],
-        "build-doc": [
-            "sphinx>=1.7",
-            "sphinx_rtd_theme",
-            "sphinx-toggleprompt",
-        ],
+        "build-doc": ["sphinx>=1.7", "sphinx_rtd_theme", "sphinx-toggleprompt"],
     },
     package_data={
         "lumispy": ["*.py", "hyperspy_extension.yaml"],
