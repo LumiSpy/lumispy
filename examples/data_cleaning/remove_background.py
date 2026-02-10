5973e24c9344968ee4c37aa9fb01fddd2f3ce5c4
janisschumacher <janisschumacher13@gmail.com>
2026-02-10 13:02:26 +0100

    modified data cleaning examples and added plotting examples

--- examples/data_cleaning/remove_background.py
+++ examples/data_cleaning/remove_background.py
@@ -5,30 +5,27 @@ Remove Background
 This example removes the background from a dataset.
 """
 
-import hyperspy.api as hs
+# %%
+# Load the data
 import lumispy as lum
 import matplotlib.pyplot as plt
-import numpy as np
-from pathlib import Path
 
-# Load the data from a file
-file_path = Path.cwd().parent.parent / "lumispy" / "data" / "asymmetric_peak_map.hspy"
+cl1 = lum.data.asymmetric_peak_map()
 
-cl1 = hs.load(file_path)
-
+# %%
 # Display the original data
 
 cl1.plot()
 plt.close(1)
 
-# Now we remove the background
+# %%
+# Now we remove the background and display the data
 
 cl2 = cl1.remove_background(signal_range=(550.0, 620.0), background_type="Offset")
 
-# Display the background removed data
-
 cl2.plot()
-plt.close(3)
+plt.close(1)
 
-# %%
+# sphinx_gallery_start_ignore
 # sphinx_gallery_thumbnail_number = 2
+# sphinx_gallery_end_ignore
