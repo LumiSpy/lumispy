593193a353ebf5b1a5972e4f9d452ee8bfc33738
janisschumacher <janisschumacher13@gmail.com>
2026-05-21 11:35:48 +0200

    changed links and updated required hyperspy version

--- lumispy/signal_tools/_selector.py
+++ lumispy/signal_tools/_selector.py
@@ -14,7 +14,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with LumiSpy. If not, see <https://www.gnu.org/licenses/#GPL>.
+# along with LumiSpy. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
 
 """
 Selector class
